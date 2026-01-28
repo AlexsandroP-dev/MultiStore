b8dc94699e6f49624b22c1154673465625465fbf
AlexsandroP-dev <alexsandrogas@gmail.com>
2026-01-28 14:49:46 -0300

    style: Migrar frontend de Tailwind para Bootstrap, views de botoes utilitarios

---
+++ documentation.md
@@ -0,0 +1,10 @@
+### 1. Checklist de Implementação (Ordem Sugerida)
+
+-   [ ] **Fase 1: Setup do Ambiente e Estrutura Base**
+    -   [x] 1.1. Iniciar projeto Laravel 12 e configurar o Docker.
+    -   [x] 1.2. Configurar conexão com o banco de dadps PostgreSQL (`.env`).
+    -   [x] 1.3. Publicar rotas, controllers e views de login padrão Breeze.
+    -   [x] 1.4. Migrar frontend de Tailwind Css para Bootstrap.
+
+-   [ ] **Fase 2: Estrutura Tema**
+    -   [x] 1.1. Views de botões utilitários.
---
+++ resources/views/utils/search/index.blade.php
@@ -0,0 +1,41 @@
+@php
+    $type = isset($type) && $type === 'card' ? 'card' : 'modal';
+@endphp
+
+@extends('utils.search.' . $type)
+
+@section('form')
+    <div class="row">
+        @yield('search')
+    </div>
+    @if (isset($between))
+        <div class="row">
+            <div class="form-group col-lg-6">
+                <label class="form-label" for="created_from">Cadastrado a partir do dia:</label>
+                <input type="date" name="created_from" id="created_from" value="{{ $_GET['created_from'] ?? '' }}"
+                    class="form-control">
+            </div>
+            <div class="form-group col-lg-6">
+                <label class="form-label" for="created_to">Cadastrado até o dia:</label>
+                <input type="date" name="created_to" id="created_to" value="{{ $_GET['created_to'] ?? '' }}"
+                    class="form-control">
+            </div>
+        </div>
+    @endif
+@stop
+
+@section('buttons')
+    <div class="form-group col-lg-3">
+        <div class="input-group">
+            <div class="input-group-text">
+                <span>QTD</span>
+            </div>
+            <input type="number" name="qtd" id="qtd" value="{{ $_GET['qtd'] ?? 100 }}"
+                class="form-control form-control-sm" step="50" minlength="50" maxlength="500">
+        </div>
+    </div>
+    <div class="btn-list">
+        <a href="{{ request()->url() }}" class="btn btn-sm btn-outline-dark">Redefinir</a>
+        @include('utils.buttons.submit', ['text' => 'Pesquisar', 'csrf' => false])
+    </div>
+@stop
